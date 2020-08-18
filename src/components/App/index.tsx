7f398d2056e582b53590d318b4669771e1a053a7
Bruno Mariano <bsignx@gmail.com>
2020-08-18 16:18:26 -0300

    chore: initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Fala</h1>
+    </div>
+  );
+};
+
+export default App;
