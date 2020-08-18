3b0b3c6168d07610a1d7c576b3ce595f2e2e5d04
Bruno Mariano <bsignx@gmail.com>
2020-08-18 17:20:59 -0300

    feat: finished styling header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import NumberDisplay from '../NumberDisplay';
+import happyImg from '../../assets/happy.png';
+
 import './styles.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Fala</h1>
+      <div className="Header">
+        <NumberDisplay value={1} />
+        <div className="Face">
+          <div className="image-container">
+            <img src={happyImg} alt="Mary happy" />
+          </div>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
     </div>
   );
 };
